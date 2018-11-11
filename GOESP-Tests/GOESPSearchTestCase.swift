93f0d07b6f41a8dea624b1a6b8afc223caa8efb0
Olga Vyrostko <violg@yandex-team.ru>
2018-11-11 13:57:45 +0300

    fix: error2

--- GOESP-Tests/GOESPSearchTestCase.swift
+++ GOESP-Tests/GOESPSearchTestCase.swift
@@ -10,24 +10,24 @@ import XCTest
 
 final class GOESPSearchTestCase: XCTestCase {
     func testSearch2Mer() {
-        XCTAssertEqual([0, 1, 4], grammar.searchDeep2(substring: "AA"))
-        XCTAssertEqual([2], grammar.searchDeep2(substring: "AC"))
-        XCTAssertEqual([5], grammar.searchDeep2(substring: "AB"))
-        XCTAssertEqual([6], grammar.searchDeep2(substring: "BA"))
+        XCTAssertEqual([0, 1, 4], grammar.searchDeep2(substring: "AA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
+        XCTAssertEqual([2], grammar.searchDeep2(substring: "AC", nodeSelectHandler: nodeDidSelect(level:symbol:)))
+        XCTAssertEqual([5], grammar.searchDeep2(substring: "AB", nodeSelectHandler: nodeDidSelect(level:symbol:)))
+        XCTAssertEqual([6], grammar.searchDeep2(substring: "BA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
 
-        XCTAssertEqual([0, 2, 5, 7, 9], grammar2.searchDeep2(substring: "CA"))
+        XCTAssertEqual([0, 2, 5, 7, 9], grammar2.searchDeep2(substring: "CA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
         
     }
 
     func testSearch3Mer() {
-        XCTAssertEqual([3], grammar.searchDeep2(substring: "CAA"))
-        XCTAssertEqual([5], grammar.searchDeep2(substring: "ABA"))
+        XCTAssertEqual([3], grammar.searchDeep2(substring: "CAA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
+        XCTAssertEqual([5], grammar.searchDeep2(substring: "ABA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
 
-        XCTAssertEqual([0, 5, 7], grammar2.searchDeep2(substring: "CAC"))
+        XCTAssertEqual([0, 5, 7], grammar2.searchDeep2(substring: "CAC", nodeSelectHandler: nodeDidSelect(level:symbol:)))
     }
 
     func testSearchEqual() {
-        XCTAssertEqual([0], grammar2.searchDeep2(substring: "CACABCACACA"))
+        XCTAssertEqual([0], grammar2.searchDeep2(substring: "CACABCACACA", nodeSelectHandler: nodeDidSelect(level:symbol:)))
     }
 
     func testSearchOnRandomStrings() {
@@ -40,19 +40,22 @@ final class GOESPSearchTestCase: XCTestCase {
             str.append(alph[randIdx])
 
             let substr = "AAC"
-            if str.indicesOf(string: substr) != grammar.searchDeep2(substring: substr) {
+            if str.indicesOf(string: substr) != grammar.searchDeep2(substring: substr, nodeSelectHandler: nodeDidSelect(level:symbol:)) {
                 print(str)
             }
         }
     }
 
     func testString() {
-        let str = "ACCACACCAACC"
+        let str = "ACCAACCCCACCAACAAAACAACA"
         let grammar = GOESP.build(str: str)
         XCTAssertEqual(str, grammar.product())
-        print(grammar.searchDeep2(substring: "AAC"))
+        print(grammar.searchDeep2(substring: "AAC", nodeSelectHandler: nodeDidSelect(level:symbol:)))
         print(str.indicesOf(string: "AAC"))
+    }
 
+    func nodeDidSelect(level: Int, symbol: Int) {
+        //print("Move to \(level), \(symbol)")
     }
 }
 
